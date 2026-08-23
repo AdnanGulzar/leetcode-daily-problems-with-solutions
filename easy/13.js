562b3d2ef874d8f567b3375a4058d344fac15352
AdnanGulzar <adnangul9900@gmail.com>
2026-08-23 06:52:13 +0000

    Add solution for 13. Roman to Integer

---
+++ easy/13.js
@@ -0,0 +1,22 @@
+// 13. Roman to Integer
+// Given a roman numeral, convert it to an integer.
+
+// solution
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const values = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    let total = 0;
+    for (let i = 0; i < s.length; i++) {
+        const value = values[s[i]];
+        if (i + 1 < s.length && value < values[s[i + 1]]) {
+            total -= value;
+        } else {
+            total += value;
+        }
+    }
+    return total;
+};
