74ed969b820097b922e9322967c06df157bd740a
AdnanGulzar <adnangul9900@gmail.com>
2026-08-31 13:24:12 +0000

    Add solution for 2. Add Two Numbers

---
+++ medium/2.js
@@ -0,0 +1,31 @@
+// 2. Add Two Numbers
+// You are given two non-empty linked lists representing two non-negative integers, with digits stored in reverse order. Add the two numbers and return the sum as a linked list.
+
+// solution
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    const dummy = new ListNode(0);
+    let current = dummy;
+    let carry = 0;
+    while (l1 || l2 || carry) {
+        const sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+    return dummy.next;
+};
