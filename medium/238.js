bbffe645527111b6f2342de0fe68b9e245033eba
AdnanGulzar <adnangul9900@gmail.com>
2026-09-04 11:03:03 +0000

    Add solution for 238. Product of Array Except Self

---
+++ medium/238.js
@@ -0,0 +1,24 @@
+// 238. Product of Array Except Self
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i], without using the division operation.
+
+// solution
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const n = nums.length;
+    const result = new Array(n).fill(1);
+    let prefix = 1;
+    for (let i = 0; i < n; i++) {
+        result[i] = prefix;
+        prefix *= nums[i];
+    }
+    let suffix = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        result[i] *= suffix;
+        suffix *= nums[i];
+    }
+    return result;
+};
